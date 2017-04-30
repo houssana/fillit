4280f5dbe89743ebbac673f5b31d4b1545b91e2f
Hamid OUSSANA <houssana@e2r8p21.42.fr>
2017-04-30 16:10:25 +0200

    pass to reinier

--- solve.c
+++ solve.c
@@ -6,7 +6,7 @@
 /*   By: rbullain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/27 11:58:30 by rbullain          #+#    #+#             */
-/*   Updated: 2017/04/28 17:16:27 by houssana         ###   ########.fr       */
+/*   Updated: 2017/04/30 15:46:27 by houssana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 /*
 ** description:
 */
+int						optimal;
+long long int			counter;
 
 static void	quit_tetrim(t_board *cur, int i, int j, t_tetrim *tetrim)
 {
@@ -64,8 +66,35 @@ static int	is_valid(t_board *cur, int i, int j, t_tetrim *tetrim)
 /*
 ** description:
 */
+static int	is_optimal(t_board *sol)
+{
+	int		nb_tetri;
+	int		sqrt;
+	int		i;
+	int		j;
 
-static void	fillit_rcs(t_board *cur, t_board *sol, t_tetrim **tetrims, int pos)
+	if (optimal)
+		return (1);
+	i = -1;
+	sqrt = 2;
+	nb_tetri = sol->size / 2 - 1;
+	while (sqrt * sqrt < nb_tetri * 4)
+		sqrt++;
+	if (sol->square > sqrt)
+		return (0);
+	while (++i < sol->square)
+	{
+		j = -1;
+		while (++j < sol->square)
+		{
+			if (sol->map[i][j] == 'A' + nb_tetri - 1)
+				return (1);
+		}
+	}
+	return (0);
+}
+
+static int	fillit_rcs(t_board *cur, t_board *sol, t_tetrim **tetrims, int pos)
 {
 	int i;
 	int j;
@@ -73,25 +102,30 @@ static void	fillit_rcs(t_board *cur, t_board *sol, t_tetrim **tetrims, int pos)
 	if (pos == (cur->size / 2) - 1)
 	{
 		if ((cur->square = calc_square(cur)) < sol->square)
-			clone_board(sol, cur);
-		return ;
-	}
-	i = 0;
-	while (i < cur->square)
-	{
-		j = 0;
-		while (j < cur->square)
 		{
-			if (is_valid(cur, i, j, tetrims[pos]) == 1)
+			clone_board(sol, cur);
+			optimal = is_optimal(sol);
+		}
+		return (1);
+	}
+	i = -1;
+	while (++i < cur->square)
+	{
+		j = -1;
+		while (++j < cur->square)
+		{
+	//		if (optimal)
+	//			return ;
+			if(is_valid(cur, i, j, tetrims[pos]))
 			{
 				if (calc_square(cur) < sol->square)
-					fillit_rcs(cur, sol, tetrims, pos + 1);
+					if (fillit_rcs(cur, sol, tetrims, pos + 1) && optimal)
+						return (1);
 				quit_tetrim(cur, i, j, tetrims[pos]);
 			}
-			j++;
 		}
-		i++;
 	}
+	return (0);
 }
 
 /*
@@ -102,6 +136,8 @@ void		fillit(t_board *solution, t_tetrim **tetrims)
 {
 	t_board *current;
 
+	counter = 0;
+	optimal = 0;
 	current = create_board(solution->size);
 	fillit_rcs(current, solution, tetrims, 0);
 	free_board(current);
